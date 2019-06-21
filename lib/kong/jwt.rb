f53fdc938639219b9727a158f1636530b8a68ab9
= <tin.nguyen@terrralogic.com>
2019-06-21 14:39:55 +0700

    monkey_patch create method of jwt, because currently kong schema reject consumer_id in request_payload

--- lib/kong/jwt.rb
+++ lib/kong/jwt.rb
@@ -4,5 +4,15 @@ module Kong
     include BelongsToConsumer
     ATTRIBUTE_NAMES = %w(id key secret consumer_id).freeze
     API_END_POINT = '/jwt/'.freeze
+
+    def create
+      headers = { 'Content-Type' => 'application/json' }
+      except_consumer_id_hash = attributes.reject { |k, v| k == 'consumer_id' }
+      response = client.post(@api_end_point, except_consumer_id_hash, nil, headers)
+
+      init_attributes(response)
+      self
+    end
+
   end
 end
